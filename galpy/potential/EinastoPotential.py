76f9c191f7cbace9ade3474fe1fb6329288dfed6
Gabriel <gabriel.pfaffman@gmail.com>
2025-03-28 11:40:39 -0400

    update EinastoPotential.py docstring.

--- galpy/potential/EinastoPotential.py
+++ galpy/potential/EinastoPotential.py
@@ -17,16 +17,18 @@ class EinastoPotential(SphericalPotential):
 
     """
 
-    def __init__(self, amp=1.0, a=2.0, normalize=False, ro=None, vo=None):
+    def __init__(self, amp=1.0, a=2.0, n=1, normalize=False, ro=None, vo=None):
         """
         Initialize a Burkert-density potential [1]_.
 
         Parameters
         ----------
         amp : float or Quantity
-            Amplitude to be applied to the potential. Can be a Quantity with units of mass density or Gxmass density.
+            Amplitude to be applied to the potential. Can be a Quantity with units of mass.
         a : float or Quantity
             Scale radius.
+        n : float
+            Einasto index.
         normalize : bool or float, optional
             If True, normalize such that vc(1.,0.)=1., or, if given as a number, such that the force is this fraction of the force necessary to make vc(1.,0.)=1. Default is False.
         ro : float or Quantity, optional
@@ -36,14 +38,13 @@ class EinastoPotential(SphericalPotential):
 
         Notes
         -----
-        - 2013-04-10 - Written - Bovy (IAS)
-        - 2020-03-30 - Re-implemented using SphericalPotential - Bovy (UofT)
+        - 2025-03-28 - Written 
 
         References
         ----------
         .. [1] Burkert (1995), Astrophysical Journal, 447, L25. ADS: https://ui.adsabs.harvard.edu/abs/1995ApJ...447L..25B.
         """
-        SphericalPotential.__init__(self, amp=amp, ro=ro, vo=vo, amp_units="density")
+        SphericalPotential.__init__(self, amp=amp, ro=ro, vo=vo, amp_units="mass")
         a = conversion.parse_length(a, ro=self._ro, vo=self._vo)
         self.a = a
         self._scale = self.a
@@ -51,24 +52,27 @@ class EinastoPotential(SphericalPotential):
             isinstance(normalize, (int, float)) and not isinstance(normalize, bool)
         ):  # pragma: no cover
             self.normalize(normalize)
-        self.hasC = True
-        self.hasC_dxdv = True
-        self.hasC_dens = True
+        self.hasC = False
+        self.hasC_dxdv = False
+        self.hasC_dens = False
         return None
 
     def _revaluate(self, r, t=0.0):
         """Potential as a function of r and time"""
-        x = r / self.a
+        h = r / self.a
         return (
-            -(self.a**2.0)
-            * numpy.pi
-            * (
-                -numpy.pi / x
-                + 2.0 * (1.0 / x + 1) * numpy.arctan(1 / x)
-                + (1.0 / x + 1) * numpy.log((1.0 + 1.0 / x) ** 2.0 / (1.0 + 1 / x**2.0))
-                + special.xlogy(2.0 / x, 1.0 + x**2.0)
-            )
+
         )
+    # (
+    #         -(self.a**2.0)
+    #         * numpy.pi
+    #         * (
+    #             -numpy.pi / x
+    #             + 2.0 * (1.0 / x + 1) * numpy.arctan(1 / x)
+    #             + (1.0 / x + 1) * numpy.log((1.0 + 1.0 / x) ** 2.0 / (1.0 + 1 / x**2.0))
+    #             + special.xlogy(2.0 / x, 1.0 + x**2.0)
+    #         )
+    #     )
 
     # Previous way, not stable as r -> infty
     # return -self.a**2.*numpy.pi/x*(-numpy.pi+2.*(1.+x)*numpy.arctan(1/x)
